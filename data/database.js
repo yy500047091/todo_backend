b8f062b610ac765088fbc06a143b7dd839facdbf
yogesh <yy500047091@gmail.com>
2023-08-07 13:20:48 +0530

    fixed engine

--- data/database.js
+++ data/database.js
@@ -4,8 +4,7 @@ import mongoose from "mongoose";
 export const connectDB = () => {
 mongoose.
  connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+  dbName: "backendapi",
 })
   .then((c) => console.log('Database Connected with ${c.connection.host}'))
   .catch((e) => console.log(e))
